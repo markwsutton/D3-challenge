866bef704a63a84ec797c57796b9ff5c71434d66
Mark W. Sutton <mark.sutton@presidio.edu>
2020-09-13 10:50:17 -0700

    Updated csv filepath

--- static/js/index.js
+++ static/js/index.js
@@ -300,7 +300,7 @@ function visualize(data){
 }
 
 // Import csv data into the variable called data passed in the function
-d3.csv("../data/data.csv").then(function(data){
+d3.csv("../static/data/data.csv").then(function(data){
     visualize(data)
 })
 
